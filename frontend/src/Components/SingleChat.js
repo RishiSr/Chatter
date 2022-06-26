2a177086ea2986be2560853bd65aff41d506a0c0
Rishi <rishisr.9044@gmail.com>
2022-06-26 17:26:09 +0530

    minor changes

--- frontend/src/Components/SingleChat.js
+++ frontend/src/Components/SingleChat.js
@@ -10,7 +10,7 @@ import { useEffect, useState } from "react";
 import './style.css'
 import ScrollableChat from "./ScrollableChat";
 import io from "socket.io-client"
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://chatter-chat-web-app.herokuapp.com/";
 var socket, selectedChatCompare;
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 
